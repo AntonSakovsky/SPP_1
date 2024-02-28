e30a6617f1d02b1afbf1bc96bda9dfdf983d31a2
AntonSaky <anton.sakovsky@gmail.com>
2024-02-29 01:04:26 +0300

    add test for xml serialization

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -2,6 +2,37 @@ namespace UnitTests
 {
     public class UnitTest1
     {
-        
+        [Fact]
+        public async void XmlSerializationTest()
+        {
+            Output resultOutput = new Output();
+            Tracer.Tracer tracer = new Tracer.Tracer(resultOutput);
+
+            SomeClass someClass = new SomeClass(tracer);
+
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => someClass.MyMethod(0));
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            string result = tracer.GetTraceResult().toXML();
+            resultOutput.FileOutput(tracer.GetTraceResult().toXML(), "test.xml");
+            string fileText;
+            using (var reader = new StreamReader("test.xml"))
+            {
+                fileText = await reader.ReadToEndAsync();
+            }
+            Assert.Equal(fileText, result);
+        }
+
+       
     }
 }
