09ad4b1bfcd07e6a3eab70f44fa278ee5e5aaea5
AntonSaky <anton.sakovsky@gmail.com>
2024-02-29 01:04:56 +0300

    add test for json serialization

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -33,6 +33,35 @@ namespace UnitTests
             Assert.Equal(fileText, result);
         }
 
-       
+        [Fact]
+        public async void JsonSerializationTest()
+        {
+            Output resultOutput = new Output();
+            Tracer.Tracer tracer = new Tracer.Tracer(resultOutput);
+
+            SomeClass someClass = new SomeClass(tracer);
+
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => someClass.MyMethod(0));
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            string result = tracer.GetTraceResult().toJSON();
+            resultOutput.FileOutput(tracer.GetTraceResult().toJSON(), "test.json");
+            string fileText;
+            using (var reader = new StreamReader("test.json"))
+            {
+                fileText = await reader.ReadToEndAsync();
+            }
+            Assert.Equal(fileText, result);
+        }
     }
 }
